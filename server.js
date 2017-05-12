c6bc072612a8a70472af89d0c5ed96e6e41fde3c
andyreesecups <andyreesecups@yahoo.com>
2017-05-11 19:06:37 -0500

    added dependencies

--- server.js
+++ server.js
@@ -2,6 +2,16 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : '',
+  password : '',
+  database : 'planner_db'
+});
+ 
+connection.connect();
 
 
 // Sets up the Express App
